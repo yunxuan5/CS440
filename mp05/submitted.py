50695e2ee5c2770767a448e9068301d80e4c6f77
Ethan Yang <ethanyyx@Ethans-MacBook-Air.local>
2023-02-27 22:04:08 -0600

    update

--- mp05/submitted.py
+++ mp05/submitted.py
@@ -18,6 +18,8 @@ files and classes when code is run, so be careful to not modify anything else.
 # maze is a Maze object based on the maze from the file specified by input filename
 # searchMethod is the search method specified by --method flag (bfs,dfs,astar,astar_multi)
 
+from queue import PriorityQueue
+
 def bfs(maze):
     """
     Runs BFS for part 1 of the assignment.
@@ -69,7 +71,42 @@ def astar_single(maze):
     @return path: a list of tuples containing the coordinates of each state in the computed path
     """
     #TODO: Implement astar_single
-
+    path = []
+    start = maze.start
+    target = maze.waypoints[0]
+    prev = dict()
+    distance = {start: 0}
+    frontier = PriorityQueue()
+    frontier.put((0, start))
+    while frontier:
+        node = frontier.get(1)
+        print(node)
+        priority, current = node    #get current location and priority from node
+        # print(current)
+        # print(current[0])
+        # print(current[1])
+        # print(priority)
+        if current == target:
+            #back trace to get the path
+            path.append(current)
+            temp = current
+            while prev[temp] != start:
+                path.append(prev[temp])
+                temp = prev[temp]
+            path.append(start)
+            path.reverse()
+            return path
+        # print(current)
+        # print(current[0])
+        # print(current[1])
+        # print(priority)
+        for neighbors in maze.neighbors(current[0], current[1]):
+            newDis = distance[current] + 1
+            if(neighbors not in distance or newDis < distance[neighbors]):
+                distance[neighbors] = newDis
+                priority = newDis + abs(target[0] - neighbors[0]) + abs(target[1] - neighbors[1])
+                frontier.put(priority, neighbors)
+                prev[neighbors] = current
     return []
 
 # This function is for Extra Credits, please begin this part after finishing previous two functions
@@ -82,5 +119,5 @@ def astar_multiple(maze):
 
     @return path: a list of tuples containing the coordinates of each state in the computed path
     """
-
+    
     return []
